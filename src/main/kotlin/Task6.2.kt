499f18e1b1685c3e56e9a9636c9970a2f1fec0f8
Artem Pavlichenko <artem.pvl@gmail.com>
2022-02-13 16:04:16 +0300

    Task 6.2 done

---
+++ src/main/kotlin/Task6.2.kt
@@ -0,0 +1,72 @@
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+fun main() {
+    val figures = arrayOf(
+        Square(10.0),
+        Parallelogram(10.0, 5.0, 90.0),
+        Circle(20.0),
+        Triangle(4.0, 4.0, 4.0)
+    )
+
+    for (fig in figures) {
+        println("For ${fig.name} area is: ${fig.area()}, and perimetr is: ${fig.perimetr()}")
+    }
+}
+
+interface Shapable {
+    val name: String
+
+    fun area(): Double
+    fun perimetr(): Double
+}
+
+class Square(var side: Double) : Shapable {
+    override val name = "Square"
+
+    override fun area(): Double {
+        return side * side
+    }
+
+    override fun perimetr(): Double {
+        return 4 * side
+    }
+}
+
+class Parallelogram(var side1: Double, var side2: Double, var angle: Double) : Shapable {
+    override val name = "Parallelogram"
+
+    override fun area(): Double {
+        return side1 * side2 * sin(angle * PI / 180)
+    }
+
+    override fun perimetr(): Double {
+        return (side1 + side2) * 2
+    }
+}
+
+class Circle(var radius: Double) : Shapable {
+    override val name = "Circle"
+
+    override fun area(): Double {
+        return PI * sqr(radius)
+    }
+
+    override fun perimetr(): Double {
+        return 2 * radius * PI
+    }
+}
+
+class Triangle(var side1: Double, var side2: Double, var side3: Double) : Shapable {
+    override val name = "Triangle"
+
+    override fun area(): Double {
+        val halfPerimetr = perimetr() / 2.0
+        return sqrt(halfPerimetr * (halfPerimetr - side1) * (halfPerimetr - side2) * (halfPerimetr - side3))
+    }
+
+    override fun perimetr(): Double {
+        return side1 + side2 + side3
+    }
+}
