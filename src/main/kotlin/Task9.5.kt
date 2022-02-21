4d9d580d92b50e2075f76c4ad5a7cd950a287f6c
Artem Pavlichenko <artem.pvl@gmail.com>
2022-02-21 21:48:04 +0300

    Task 9.5 refactored

--- src/main/kotlin/Task9.5.kt
+++ src/main/kotlin/Task9.5.kt
@@ -1,22 +1,21 @@
 fun main() {
     val countryCode = "+7"
-    val phoneLength = 10
     var phoneBook = mutableMapOf(
         "Vova" to "+79004003020",
         "Tanya" to "+79054001022",
-        "Ted" to "+709004003020",
+        "Ted" to "+70900400302",
         "Dasha" to "+357005001020",
         "Nobody" to "",
         "Petya" to "+17005001020",
         "Marina" to "+127005001020",
     )
 
-    println("phone book: $phoneBook\n")
+    println("phone book: $phoneBook")
+    println("country code: $countryCode\n")
 
     val countryCodeRange = IntRange(0, countryCode.length - 1)
-    phoneBook = phoneBook.filterValues {
-        (it.length == countryCode.length + phoneLength) &&
-                (it.slice(countryCodeRange) == countryCode)
+    phoneBook = phoneBook.filterValues { it ->
+        (it.length >= countryCode.length) && (it.slice(countryCodeRange) == countryCode)
     }.toMutableMap()
 
     println("filtered phone book: $phoneBook")
