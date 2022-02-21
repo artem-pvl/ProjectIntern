b6b41a356096b493ac4c991c5ea0cd33a1b66783
Artem Pavlichenko <artem.pvl@gmail.com>
2022-02-21 21:25:47 +0300

    Task 9.3 val name changed (arrayOfNumbers)

--- src/main/kotlin/Task9.3.kt
+++ src/main/kotlin/Task9.3.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val arrayOfNumbers = List(10) { (-50..50).random() }
-    println(arrayOfNumbers)
-    val sqrList = arrayOfNumbers.map { it*it }
+    val listOfNumbers = List(10) { (-50..50).random() }
+    println(listOfNumbers)
+    val sqrList = listOfNumbers.map { it*it }
     println(sqrList)
 }
