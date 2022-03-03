626800bbd5b7944fbb920cb2be248bdb668f5e4e
Artem Pavlichenko <artem.pvl@gmail.com>
2022-03-03 22:57:45 +0300

    Task 11.1: new line after "->" added

--- src/main/kotlin/Task11.1.kt
+++ src/main/kotlin/Task11.1.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val fahrenheit = {celsius: Int -> celsius * 9.0 / 5.0 + 32 }
+    val fahrenheit = { celsius: Int ->
+        celsius * 9.0 / 5.0 + 32
+    }
     val celsius = 25
     println("$celsius degrees Celsius is ${fahrenheit(celsius)} degrees Fahrenheit")
 }
